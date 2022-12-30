80dc6aecc61d5bd18ad695ba9c99723ffb042ba6
Steven <stevenlin3263@gmail.com>
2022-12-30 23:24:09 +0800

    [feat] create the layout of banner

--- src/app/features/Features/Homepage/Homepage.tsx
+++ src/app/features/Features/Homepage/Homepage.tsx
@@ -2,7 +2,41 @@ import React from 'react';
 
 const Homepage: React.FC = () => {
   return (
-    <div>wait for developing</div>
+    <>
+      {/* Banner */}
+      <div className="w-100 banner-container">
+        <div className="banner-text w-100 h-100 text-center">
+            <p className="fs-24 color-white">Hello!!</p>
+            <p className="fs-48 color-white">I am a <span className="color-orange">Front-end developer</span></p>
+            <button className="button-white-outline">Resume</button>
+        </div>
+        <img
+          className="w-100"
+          src={require('assets/image/S__15122444.jpg')}
+          alt="banner"
+        />
+      </div>
+      {/* About me */}
+      <div className="section-gray">
+        <div className="container">
+          <div className="d-flex justify-content-center mb-3">
+            <div className="section-header">
+              About me
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <img
+                className="w-80"
+                src={require('assets/image/S__15106057.jpg')}
+                alt="steven"
+              />
+            </div>
+            <div className="col-6">2</div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 export default Homepage;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
-import './assets/scss/common.css'
+import './assets/scss/common.scss'
 
 ReactDOM.render(
   <React.StrictMode>
