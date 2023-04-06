59f6c56bbba2b147f967c133c8c2820d05674583
Steven <stevenlin3263@gmail.com>
2023-04-06 11:52:03 +0800

    [fix] Redirect to the homepage

--- src/app/features/Features/FeaturesRoutes.tsx
+++ src/app/features/Features/FeaturesRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { ROUTES } from '../../../app/core/router';
 import LazySpinner from '../../common/component/Spinner/LazySpinner';
 
@@ -11,6 +11,9 @@ const FeaturesRoutes: React.FC = () => {
           <Route
             path={ROUTES.FEATURES__HOMEPAGE}
             component={React.lazy(() => import('./Homepage'))}
+          />
+          <Redirect
+            to={ROUTES.FEATURES__HOMEPAGE}
           />
         </Switch>
       </BrowserRouter>
